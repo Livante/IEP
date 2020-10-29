b2bf0e27ac43610d3ed20d176564b439a66ca0a1
Livante <levigergelyfilevi@gmail.com>
2020-10-29 15:20:24 +0200

    Step 4-5

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,19 +1,45 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 class Myclass{
+        enum{MaxLen=255}; //enum in loc de define
+        int a[MaxLen]={0}; //toate elementele sunt initializate cu 0;
+        const int DivideByTwo=2;//constante in loc de define
+        char *p='a'; //initializare 
+        mutable std::size_t textLength=1; //initializare
+
     private:
-        enum{MaxLen=255};
-        int a[MaxLen];
-        const int DivideByTwo=2;
+        int nrPicior;
+        int varsta;
+   
     public:
-        template<typename T>
+        Myclass::Myclass(); //default constructor
+        Myclass::Myclass(const std::int& nrPicior, const std::int& varsta)
+            :nrPicior(nrPicior),
+            varsta(varsta)
+            {}
+
+        ~Myclass(); //destructor
+
+
+        template<typename T>//folosirea metodelor generice si a functiilor inline
         inline void parity(const T& a)
         {
             f(a%DivideByTwo?1:0);
         }
-        const char& operator[](std::size_t position)const
-        {
-            
-            return text[position];
+ 
+        std::size_t Myclass::length()const{
+            if(parity(2)){
+                textLength=std::strlen(p);
+            }
         }
+
+
+    int main(){
+        Myclass pers1=new Myclass(41,51); //default constructor
+        Myclass pers2(pers1); //copy constructor
+        pers2=pers1; //copy assignment operator
+    }
+
 };
