6ef7c046f453268b91386128d4521fe5a4861b5b
Livante <levigergelyfilevi@gmail.com>
2020-11-12 21:20:13 +0200

    cu Cmake, in rest not sure if it s okay

---
+++ lab5/build/CMakeFiles/proba1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proba1.dir/Box.cpp.o"
+  "proba1.pdb"
+  "proba1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proba1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
